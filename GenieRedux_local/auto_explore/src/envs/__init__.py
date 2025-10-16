1a70dda06f178b6bf0f63083eb2ac9f0d25f8489
Daino360 <stefano.dainelli@edu.unifi.it>
2025-10-16 12:10:02 +0200

    Remove temporary GenieRedux_local after subtree merge

--- GenieRedux_local/auto_explore/src/envs/__init__.py
+++
@@ -1,4 +0,0 @@
-from .multi_process_env import MultiProcessEnv
-from .wrappers import make_atari, ResizeObsWrapper, make_retro
-from .single_process_env import SingleProcessEnv
-from .retrowrapper import RetroWrapper
--- GenieRedux_local/models/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from .dynamics import Dynamics, MaskGIT
-from .genie_redux import GenieRedux, GenieReduxGuided
-from .lam import LatentActionModel
-from .tokenizer import Tokenizer
-from .construct_model import construct_model
